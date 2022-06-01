a3267007b78fe86336dd26a7a965ad37848665c9
Augusto Rodrigues de Oliveira Sardi <augusto.roct@gmail.com>
2022-06-01 12:35:34 -0300

    added env n gitignore

--- src/app/models/config/config.py
+++ src/app/models/config/config.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import firestore
-credentials = firebase_admin.credentials.Certificate("src/app/models/config/models-4ae2e-firebase-adminsdk-7v3f2-e07740eeee.json")
+credentials = firebase_admin.credentials.Certificate("src/app/models/config/env.json")
 default_app = firebase_admin.initialize_app(credentials)
 
 db = firestore.client()
